f905178f84a8c5a7dba29b8bb6b622dada5808b6
Jeet Das <ayushmandas@hotmail.com>
2018-08-23 18:17:23 -0500

    Backend cleanup

--- main.go
+++ main.go
@@ -3,67 +3,72 @@ package main
 import (
 	"log"
 	"net/http"
-	"html/template"
-
-	//"github.com/gorilla/websocket"
+	"github.com/gorilla/websocket"
 	"github.com/gorilla/mux"
 )
 
-/*
-var clients = make(map[*websocket.Conn] string)
+var clients = make(map[*websocket.Conn]bool)
 var broadcast = make(chan YTSong)
 
-// Configure the upgrader
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
 		return true
 	},
 }
-*/
-
-type Playlist struct {
-	NumberOfSongs	int 		`json"numberOfSongs"`
-	Songs			[]YTSong	`json"songs"`
-}
 
 type YTSong struct {
-	Image	string `json:"image"`
-	Title	string `json:"title"`
-	Artist	string `json:"artist"`
+	Image	string	`json:"image"`
+	Title	string	`json:"title"`
+	Artist	string	`json:"artist"`
 }
 
 func main() {
+	fs := http.FileServer(http.Dir("../public"))
+	http.Handle("/", fs)
 
-	r := mux.NewRouter()
+	http.HandleFunc("/ws", handleConnections)
 
-	r.PathPrefix("/css").Handler(http.FileServer(http.Dir("src/css/")))
-	r.PathPrefix("/js").Handler(http.FileServer(http.Dir("src/js/")))
+	go handleNewRequests()
 
-	r.HandleFunc("/ws/{id}", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		id := vars["id"]
-
-		log.Printf("idiididid %v", id)
-	})
-
-	tmpl := template.Must(template.ParseFiles("../Party-Playlist/src/index.html"))
-
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("We're in hello")
-		data := Playlist{
-			NumberOfSongs: 10,
-			Songs: []YTSong{
-				{Image: "Image 1", Title: "Song title 1", Artist: "Artist 1"},
-				{Image: "Image 1", Title: "Song title 1", Artist: "Artist 1"},
-				{Image: "Image 1", Title: "Song title 1", Artist: "Artist 1"},
-			},
-		}
-		tmpl.Execute(w, data)
-	})
-
-	log.Println("http server started on :8000")
-	err := http.ListenAndServe(":8000", r)
+	log.Println("Http server started on :8000")
+	err := http.ListenAndServe(":8000", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer ws.Close()
+
+	clients[ws] = true
+
+	for {
+		var ytsong YTSong
+		err := ws.ReadJSON(&ytsong)
+		if err != nil {
+			log.Printf("error: %v", err)
+			delete(clients, ws)
+			break
+		}
+		broadcast <- ytsong
+	}
+}
+
+func handleNewRequests() {
+	for {
+		ytsong := <-broadcast
+		for client := range clients {
+			err := client.WriteJSON(ytsong)
+			if err != nil {
+				log.Printf("error: %v", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
 	}
 }
